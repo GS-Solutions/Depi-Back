056ab9bbb4692ff8c727c511a53e3d2c2f27329c
ahmedZakariaGS <127016244+ahmedZakariaGS@users.noreply.github.com>
2024-11-27 17:23:47 +0200

    add login function

---
+++ Depi-Back/Depi-Back.Server/Controllers/Authorization/AuthenticationController.cs
@@ -0,0 +1,45 @@
+﻿ 
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MOE.School.Server.Controllers.Authorization
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class AuthenticationController : ControllerBase
+    {
+
+        [HttpPost]
+        [Route("Login")]
+        //[Authorize]
+        public async Task<ActionResult > Login()
+        {
+
+            
+
+            var empDetailsData = new
+            {
+                id = 101,
+                username = "UserGS",
+                isActive = true,
+                role = "admin"
+            };
+            if (empDetailsData != null)
+            {
+                return Ok(empDetailsData);
+
+            }
+            else
+            {
+                return Ok(null);
+
+            }
+
+
+        }
+
+
+    }
+}
